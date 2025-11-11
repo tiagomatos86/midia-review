1061b6a562d52805e059cfc9426f56e3cc2d3508
tiagomatos86 <tiago_m_lima@hotmail.com>
2025-11-10 23:39:03 -0300

    Feito correção na anotação do ano de lançamento de NotBlank para NotNull, criando o MidiaController com dois endpoints básicos para salvar uma mídia e retornar todas as mídias

--- src/main/java/com/tiago/midia_review/controllers/MidiaController.java
+++ src/main/java/com/tiago/midia_review/controllers/MidiaController.java
@@ -1,5 +1,34 @@
 package com.tiago.midia_review.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tiago.midia_review.model.Midia;
+import com.tiago.midia_review.repository.MidiaRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/midias")
 public class MidiaController { 
     
+    @Autowired
+    private MidiaRepository midiaRepository;
+
+    //GET /midias -> lista todas as mídias
+    @GetMapping
+    public List<Midia> listarTodas() {
+        return midiaRepository.findAll();
+    }
+
+    // POST /midias -> Cadastra uma nova mídia
+    @PostMapping
+    public Midia salvarMidia(@RequestBody Midia midia) {
+        return midiaRepository.save(midia);
+    }
 }
--- src/main/java/com/tiago/midia_review/model/Midia.java
+++ src/main/java/com/tiago/midia_review/model/Midia.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @Entity
@@ -27,7 +28,7 @@ public class Midia {
     @NotBlank(message = "O tipo é obrigatório")
     private String tipo;
 
-    @NotBlank(message = "O ano de lançamento é obrigatório")
+    @NotNull(message = "O ano de lançamento é obrigatório")
     private int anoLancamento;
     private String imageUrl;
 
